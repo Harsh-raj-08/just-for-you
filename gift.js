00233c2a29f588821f591b810796d0a6154d4c31
harsh raj <harshraj381017@gmail.com>
2025-02-14 16:35:13 +0530

    added effects on gift page

--- gift.js
+++ gift.js
@@ -6,6 +6,18 @@ const jhumka = document.querySelector(".jhumka");
 const chololates = document.querySelector(".chololates");
 const pandaLight = document.querySelector(".pandaLight");
 const nextBtn = document.querySelector(".next-btn");
+const textBox = document.querySelector(".text-box");
+const gifts = document.querySelector(".gift-img");
+const jhumkaText = document.querySelector(".jhumka-text");
+const jhumkaImg = document.querySelector(".jhumka-img");
+const braceletText = document.querySelector(".bracelet-text");
+const braceletImg = document.querySelector(".bracelet-img");
+const teddyText = document.querySelector(".teddy-text");
+const teddyImg = document.querySelector(".teddy-img");
+const pandaText = document.querySelector(".panda-text");
+const pandaImg = document.querySelector(".panda-img");
+const chololatesText = document.querySelector(".chololates-text");
+const chololatesImg = document.querySelector(".chololates-img");
 
 btn.addEventListener("click", () => {
   giftCap.classList.toggle("open-box");
@@ -19,9 +31,51 @@ btn.addEventListener("click", () => {
     btn.style.display = "none";
   }, 2000);
   setTimeout(() => {
+    giftCap.style.zIndex = "1";
+  }, 2000);
+  setTimeout(() => {
     nextBtn.style.display = "block";
   }, 7000);
   setTimeout(() => {
     nextBtn.classList.toggle("appear");
   }, 7000);
+});
+
+// gifts.addEventListener("mouseover", () => {
+//   textBox.classList.toggle("text-ani");
+// });
+
+teddyImg.addEventListener("mouseover", () => {
+  teddyText.classList.add("text-ani");
+});
+teddyImg.addEventListener("mouseout", () => {
+  teddyText.classList.remove("text-ani");
+});
+
+braceletImg.addEventListener("mouseover", () => {
+  braceletText.classList.add("text-ani");
+});
+braceletImg.addEventListener("mouseout", () => {
+  braceletText.classList.remove("text-ani");
+});
+
+jhumkaImg.addEventListener("mouseover", () => {
+  jhumkaText.classList.add("text-ani");
+});
+jhumkaImg.addEventListener("mouseout", () => {
+  jhumkaText.classList.remove("text-ani");
+});
+
+pandaImg.addEventListener("mouseover", () => {
+  pandaText.classList.add("text-ani");
+});
+pandaImg.addEventListener("mouseout", () => {
+  pandaText.classList.remove("text-ani");
+});
+
+chololatesImg.addEventListener("mouseover", () => {
+  chololatesText.classList.add("text-ani");
+});
+chololatesImg.addEventListener("mouseout", () => {
+  chololatesText.classList.remove("text-ani");
 });
